7c49b8ad3bc7a86c2f5b444fba91e63701c3e93a
JU <juljsyr@students.cs.ubc.ca>
2021-03-29 13:44:00 -0700

    added rest option, random stats; stats can now change with all options

--- main.pl
+++ main.pl
@@ -12,17 +12,19 @@ player_stats(F,M,C,I,S).
         
 sword :- 
         write('You study the blade *SCHWING'), nl,
-        random_between(0,1,SIGN), random_between(1,10,MAG), change_stats(0, SIGN, MAG). % change later
-book :- 
-        write('You read the Art of Wario'), nl,
-        random_between(0,1,SIGN), random(MAG), change_stats(1, SIGN, MAG).
+        random_between(0,1,SIGN), mag_generator(MAG), change_stats(0, SIGN, MAG).
 work :- 
         write('You work parttime at the local WcBonalds'), nl,
-        random_between(0,1,SIGN), random(MAG), change_stats(2, SIGN, MAG).
+        random_between(0,1,SIGN), mag_generator(MAG), change_stats(1, SIGN, MAG).
 talk :- 
         write('You practice your stand-up routine'), nl,
-        random_between(0,1,SIGN), random(MAG), change_stats(3, SIGN, MAG).
-
+        random_between(0,1,SIGN), mag_generator(MAG), change_stats(2, SIGN, MAG).
+book :- 
+        write('You read the Art of Wario'), nl,
+        random_between(0,1,SIGN), mag_generator(MAG), change_stats(3, SIGN, MAG).
+rest :-
+        write('You decide to take a nap'), nl, % yean go make some better comments
+        change_stats(4,_,_).
 
 stat :-
         getPlayerStats(F,M,C,I,S),
@@ -32,24 +34,45 @@ stat :-
         number_string(I,Istr),
         number_string(S,Sstr),
         write('Your Strength is: '), write(Fstr), nl,
-        write('Your Intelligence is: '), write(Istr), nl,
-        write('Your Charisma is: '), write(Cstr), nl,
         write('Your Wealth is: '),  write(Mstr),nl,
+        write('Your Charisma is: '), write(Cstr), nl,
+        write('Your Intelligence is: '), write(Istr), nl,
         write('Your Stamina is: '), write(Sstr), nl.
 
 getPlayerStats(F,M,C,I,S) :- 
         player_stats(F,M,C,I,S).
 
-% sword = 0, book = 1, work = 2, talk = 3
+% sword = 0, book = 1, work = 2, talk = 3, rest = 4
 % sign 0 = neg, sign 1 = pos
 change_stats(ACTION, SIGN, MAG) :-
         getPlayerStats(F,M,C,I,S),
         retract(player_stats(F,M,C,I,S)),
-        S1 is S-1,
-        (       ACTION =:= 0, SIGN =:= 0 -> F1 is F-MAG
-        ;       ACTION =:= 0, SIGN =:= 1 -> F1 is F+MAG
+        (       ACTION =:= 4 -> S1 is S+2
+        ;       ACTION =:= 0, SIGN =:= 0 -> F1 is F-MAG, S1 is S-1
+        ;       ACTION =:= 0, SIGN =:= 1 -> F1 is F+MAG, S1 is S-1
+        ;       ACTION =:= 1, SIGN =:= 0 -> M1 is M-MAG, S1 is S-1
+        ;       ACTION =:= 1, SIGN =:= 1 -> M1 is M+MAG, S1 is S-1
+        ;       ACTION =:= 2, SIGN =:= 0 -> C1 is C-MAG, S1 is S-1
+        ;       ACTION =:= 2, SIGN =:= 1 -> C1 is C+MAG, S1 is S-1
+        ;       ACTION =:= 3, SIGN =:= 0 -> I1 is I-MAG, S1 is S-1
+        ;       ACTION =:= 3, SIGN =:= 1 -> I1 is I+MAG, S1 is S-1
         ),
-        assert(player_stats(F1,M,C,I,S1)).
+        (       ACTION =:= 0 -> assert(player_stats(F1,M,C,I,S1))
+        ;       ACTION =:= 1 -> assert(player_stats(F,M1,C,I,S1))
+        ;       ACTION =:= 2 -> assert(player_stats(F,M,C1,I,S1))
+        ;       ACTION =:= 3 -> assert(player_stats(F,M,C,I1,S1))
+        ;       ACTION =:= 4 -> assert(player_stats(F,M,C,I,S1))
+        ).
+        
+
+mag_generator(RES) :-
+        random(MAG),
+        (       MAG >= 0, MAG < 0.65 -> RES is 1
+        ;       MAG >= 0.65, MAG < 0.9 -> RES is 2
+        ;       MAG >= 0.9, MAG < 1 -> RES is 4       
+        ).
+
+ 
 
 
 start :-
@@ -62,9 +85,10 @@ instructions :-
     write('You are Jonathan, the newest member of the village guild. A dragon comes to the village once a month and demands gold. Every time, the village is forced to surrender. This time, you are determined to stop it.'), nl,
     write('You hear a mysterious voice from the heavens call out to you...'), nl,
     write('sword.         -- to train your strength.'), nl,
-    write('book.          -- to train your intelligence'), nl,
     write('work.          -- to earn some money.'), nl,
     write('talk.          -- to train your charisma.'), nl,
+    write('book.          -- to train your intelligence'), nl,
+    write('rest.          -- to take a nap'), nl,
     write('stat.          -- to see your current status.'), nl,
     write('halt.          -- to end the game and quit.'), nl,
     write('What was that voice! And what does it mean to quit the game! You ask yourself these questions.'), nl,
